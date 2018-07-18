9850fc8a16a9bfb8906f0e195e2832f2b15bb194
pengyf5-lenovo-pc <pengyf5@lenovo.com>
2018-07-18 17:12:56 +0800

    添加注释

--- pages/chart/chart.js
+++ pages/chart/chart.js
@@ -8,6 +8,7 @@ Page({
         this.setData({
             params: queryBean
         });
+        //拿到去除五险一金之后剩余的工资
         if(this.data.params.custom51){
             var endowment = parseFloat(this.data.params.endowment);
             var medical = parseFloat(this.data.params.medical);
@@ -19,7 +20,9 @@ Page({
         }else{
             var moneyForTax = parseFloat(this.data.params.salary)*0.778; 
         }
+        //通过起征点计算需计算税额的部分
         var moneyForTaxStart = moneyForTax-3500;
+        //查找对应的税率和应减数
         var curTaxRate = eld_tax_table.taxIndex.filter(function(item,index,arr){
             var curTax = parseInt(item);
             var nextTax = parseInt(arr[index+1]);
@@ -27,12 +30,14 @@ Page({
                 return curTax;
             }
         })
-
-        var rate = eld_tax_table.taxObj[curTaxRate+""].rate;
-        var subNum = eld_tax_table.taxObj[curTaxRate+""].subNum;
+        if(moneyForTaxStart > 0){
+            var rate = eld_tax_table.taxObj[curTaxRate+""].rate;
+            var subNum = eld_tax_table.taxObj[curTaxRate+""].subNum;
+        }else{
+            var rate = 0;
+            var subNum = 0;
+        }
+        //计算实际到手的收入
         console.log((moneyForTax-(moneyForTaxStart*rate-subNum)).toFixed(2));
-
-
-
     }
 })
