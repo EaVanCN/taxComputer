b061ea977bb25c5e4ac1a38028b7fe78a5b3cab7
PC-20170708ZWMK\Administrator <pengyifan93@live.com>
2018-07-20 21:41:36 +0800

    temp submit

--- pages/chart/chart.js
+++ pages/chart/chart.js
@@ -1,4 +1,5 @@
 var eld_tax_table = require("./../eld_tax_table");
+import * as echarts from '../../ec-canvas/echarts';
 Page({
     data: {
         params: {}
@@ -8,6 +9,7 @@ Page({
         this.setData({
             params: queryBean
         });
+        //拿到去除五险一金之后剩余的工资
         if(this.data.params.custom51){
             var endowment = parseFloat(this.data.params.endowment);
             var medical = parseFloat(this.data.params.medical);
@@ -19,18 +21,27 @@ Page({
         }else{
             var moneyForTax = parseFloat(this.data.params.salary)*0.778; 
         }
-        var moneyForTaxStart = moneyForTax-3500;
-        var curTaxRate = eld_tax_table.taxIndex.filter(function(item,index,arr){
-            var curTax = parseInt(item);
-            var nextTax = parseInt(arr[index+1]);
-            if(nextTax && moneyForTaxStart>curTax && moneyForTaxStart<=nextTax){
-                return curTax;
-            }
-        })
-
-        var rate = eld_tax_table.taxObj[curTaxRate+""].rate;
-        var subNum = eld_tax_table.taxObj[curTaxRate+""].subNum;
+        //通过起征点计算需计算税额的部分
+        var moneyForTaxStart = moneyForTax-3500 > 0 ? moneyForTax-3500 : moneyForTax;
+        //查找对应的税率和应减数
+        if(moneyForTax-3500 > 0){
+            var curTaxRate = eld_tax_table.taxIndex.filter(function(item,index,arr){
+                var curTax = parseInt(item);
+                var nextTax = parseInt(arr[index+1]);
+                if(nextTax && moneyForTaxStart>curTax && moneyForTaxStart<=nextTax){
+                    return curTax;
+                }
+            })
+            var rate = eld_tax_table.taxObj[curTaxRate+""].rate;
+            var subNum = eld_tax_table.taxObj[curTaxRate+""].subNum;
+        }else{
+            var rate = 0;
+            var subNum = 0;
+        }
+        //计算实际到手的收入
         console.log((moneyForTax-(moneyForTaxStart*rate-subNum)).toFixed(2));
+        
+
 
 
 
